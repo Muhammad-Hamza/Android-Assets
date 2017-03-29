243200f3694afd011e85c53273d00d933611fa75
ERO-SOL-PC-04 <muhammad00hamza@gmail.com>
2017-03-29 16:18:40 +0500

    android assets

---
+++ app/src/main/java/com/example/ero_sol_pc_04/androidassets/AssetLinks.java
@@ -0,0 +1,75 @@
+//package com.example.ero_sol_pc_04.androidassets;
+//
+///**
+// * Created by ERO-SOL-PC-04 on 3/29/2017.
+// */
+//
+//public class AssetLinks {
+//    Tutorials
+//    http://sampleprogramz.com/android/seekbar.php
+//
+//
+//    RecyclerView With Filter
+//    https://www.youtube.com/watch?annotation_id=annotation_724446373&feature=iv&src_vid=j9_hcfWVkIc&v=j9_hcfWVkIc#t=20m30s
+//
+//    Android
+//    https://www.youtube.com/watch?v=-iog_fmm6mE
+//
+//
+//    ReplyNotification
+//    http://www.feelzdroid.com/2017/01/android-n-inline-reply-push-notifications.html
+//    Image Notification
+//    http://www.feelzdroid.com/2015/09/show-bigpicturestyle-notification-android-example.html
+//
+//
+//    Image Gallery App
+//    http://www.androidhive.info/2016/04/android-glide-image-library-building-image-gallery-app/
+//
+//    Best Example RecyclerView
+//    https://guides.codepath.com/android/using-the-recyclerview
+//
+//    Dialogue Fragment Back Button
+//    http://stackoverflow.com/questions/24134714/up-actionbar-action-on-dialogfragment?noredirect=1&lq=1
+//
+//    http://stackoverflow.com/questions/22754775/how-to-add-action-bar-to-a-dialogfragment
+//
+//    GridLayout Span Size https://github.com/luizgrp/SectionedRecyclerViewAdapter/blob/master/app/src/main/java/io/github/luizgrp/sectionedrecyclerviewadapter/demo/Example1Fragment.java
+//
+//
+//    Retrieve all Contacts from Phone
+//    http://sonevalley.blogspot.com/2015/12/retrieving-list-of-contacts-in-android.html
+//
+//    Make a Grid Header in Recyclrview
+//    https://www.youtube.com/watch?v=VGjRDZtMXM0
+//
+//    Mute Voice On a click
+//    http://stackoverflow.com/questions/13245673/how-to-turn-off-phone-ringer-for-incoming-calls
+//
+//    Divider In List RecyclerView
+//    https://www.bignerdranch.com/blog/a-view-divided-adding-dividers-to-your-recyclerview-with-itemdecoration/
+//
+//    Motion Events and movements
+//    http://stackoverflow.com/questions/3148741/how-to-capture-finger-movement-direction-in-android-phone
+//
+//    Collapsing Toolbar Layout Example
+//    http://www.zoftino.com/collapsing-toolbar-layout-example
+//
+//
+//    Horizontal recyclerView
+//    http://whats-online.info/science-and-tutorials/87/Android-tutorial-Horizontal-RecyclerView-with-images-and-text-example/
+//
+//    Sliding To cancel
+//    http://stackoverflow.com/questions/39303903/slide-to-cancel-animation-in-android
+//
+//    Log in Screen TextInputLayout
+//    https://code.tutsplus.com/tutorials/creating-a-login-screen-using-textinputlayout--cms-24168
+//
+//    RecyclerView Best Tutorial
+//    https://enoent.fr/blog/2015/01/18/recyclerview-basics/
+//
+//    vertical TextView
+//    https://blog.stylingandroid.com/verticaltext-part-1/
+//
+//    Hide toolbar Animation
+//    http://stackoverflow.com/questions/19765938/show-and-hide-a-view-with-a-slide-up-down-animation
+//}
--- app/src/main/java/com/example/ero_sol_pc_04/androidassets/attachmentdialoguesettingdialogpositionthroughjava.java
+++ app/src/main/java/com/example/ero_sol_pc_04/androidassets/attachmentdialoguesettingdialogpositionthroughjava.java
@@ -1,51 +1,51 @@
-package com.erosol.voip.dialogs;
-
-import android.app.Dialog;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.DialogFragment;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
-
-import com.erosol.voip.R;
-
-
-public class AttachmentDialog extends DialogFragment {
-
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_attachment_dialogue,container,false);
-
-        Window window = getDialog().getWindow();
-        window.setGravity(Gravity.BOTTOM);
-
-        return rootView;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        Dialog d = getDialog();
-        if (d!=null){
-            int width = ViewGroup.LayoutParams.MATCH_PARENT;
-            int height = 400;
-            d.getWindow().setLayout(width, height);
-        }
-    }
-
-    @Override
-    public void onResume() {
-        final Window dialogWindow = getDialog().getWindow();
-        WindowManager.LayoutParams lp = dialogWindow.getAttributes();
-        lp.y = 200;        // set Y position here
-        dialogWindow.setAttributes(lp);
-
-        super.onResume();
-    }
-}
+//package com.erosol.voip.dialogs;
+//
+//import android.app.Dialog;
+//import android.os.Bundle;
+//import android.support.annotation.Nullable;
+//import android.support.v4.app.DialogFragment;
+//import android.view.Gravity;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.view.Window;
+//import android.view.WindowManager;
+//
+//import com.erosol.voip.R;
+//
+//
+//public class AttachmentDialog extends DialogFragment {
+//
+//
+//    @Nullable
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        View rootView = inflater.inflate(R.layout.fragment_attachment_dialogue,container,false);
+//
+//        Window window = getDialog().getWindow();
+//        window.setGravity(Gravity.BOTTOM);
+//
+//        return rootView;
+//    }
+//
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        Dialog d = getDialog();
+//        if (d!=null){
+//            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+//            int height = 400;
+//            d.getWindow().setLayout(width, height);
+//        }
+//    }
+//
+//    @Override
+//    public void onResume() {
+//        final Window dialogWindow = getDialog().getWindow();
+//        WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+//        lp.y = 200;        // set Y position here
+//        dialogWindow.setAttributes(lp);
+//
+//        super.onResume();
+//    }
+//}
